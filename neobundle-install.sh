1349d8c3ea43ef46eb59ef6f5d2c90f11c74df17
Daniel Strong <dstrong@glyx.co.uk>
2015-10-17 17:11:20 +0100

    Add some helpful output to neobundle-install

--- neobundle-install.sh
+++ neobundle-install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
 if [ ! -e ~/.vim/bundle/neobundle.vim ]; then
+    echo "Installing NeoBundle"
     curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+    echo "Done!"
 else
     echo "NeoBundle already installed."
 fi
