a4c4b65daa483fe6d524149818621e5bfc315cfa
Daniel Strong <dstrong@glyx.co.uk>
2018-01-11 14:55:33 +0000

    Switch to gopass

--- aliases.zsh
+++ aliases.zsh
@@ -7,15 +7,6 @@ dn () {
     python -c "import random; print(random.randrange(1, $1 + 1))"
 }
 
-SECONDARY_PASS_DIR="$HOME/.mypass"
-if [[ -d "$SECONDARY_PASS_DIR" ]]; then
-    mypass () {
-        PASSWORD_STORE_DIR=~/.mypass pass "$@"
-    }
-
-    alias mypcp='mypass show -c'
-fi
-
 # Die aliases
 for n in $(seq 2 20); do
   alias d$n="dn $n"
@@ -36,12 +27,13 @@ alias t='true'
 # Common opts
 alias grep='grep --color=auto'
 alias ls='ls --color=auto'
-alias pcp='pass show -c'
+alias pcp='gopass show -c'
 alias xclip='xclip -selection clipboard'
 
 # Alternative names
 alias loc='tokei'
 alias nc='ncat'
+alias pass='gopass'
 alias vim='nvim'
 
 # Docker
