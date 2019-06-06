ff3e837f851f0c2f1441719d6b039f9f887eed0e
Daniel Strong <dstrong@glyx.co.uk>
2019-06-06 14:22:02 +0100

    Remove vundle stuff

--- vundle-install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-if [ ! -e ~/.config/nvim/bundle/Vundle.vim ]; then
-    echo "Cloning Vundle."
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
-else
-    echo "Vundle already installed."
-fi
